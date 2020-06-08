3c7a0dba145c10312e7a47e9b3accbbd0edc3b60
carloscuenca <cuenca.cl@gmail.com>
2020-06-08 12:43:37 -0700

    add magicalWell

--- LoopTunnel.php
+++ LoopTunnel.php
@@ -48,3 +48,22 @@ function countSumOfTwoRepresentations2($n, $l, $r) {
 	return $count;
 
 }
+
+function magicalWell($a, $b, $n) {
+    
+
+    $salary = 0;
+
+    while($n > 0) {
+
+        $salary += $a * $b;
+
+        $a++;
+        $b++;
+        $n--;
+
+    }
+
+    return $salary;
+
+}
